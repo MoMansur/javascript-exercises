ea865af70d56363d2c85546254ac75a9416c3a35
MoMansur <mansurshehumsm@gmail.com>
2024-06-06 15:16:38 +0300

    02_repeatString Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+
+//    return str.repeat(num)
+if(num === -1){
+    return 'ERROR'
+}else{
+    return str.repeat(num)
+}
 };
 
 // Do not edit below this line
